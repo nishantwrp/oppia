5dc938027bc22519263bb6fe568eef358a69129d
Nishant Mittal <mittalnishant14@outlook.com>
2020-04-04 21:12:44 +0530

    CKEDITOR REMOVED

--- core/templates/components/ck-editor-helpers/ck-editor-4-rte.directive.ts
+++ core/templates/components/ck-editor-helpers/ck-editor-4-rte.directive.ts
@@ -16,7 +16,6 @@
  * @fileoverview Directive for CK Editor.
  */
 
-require('third-party-imports/ckeditor.import.ts');
 require('services/context.service.ts');
 require('services/rte-helper.service.ts');
 

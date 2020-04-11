a30da231ae02c9366f8716c74c9cdfff2d654efa
Nishant Mittal <mittalnishant14@outlook.com>
2020-04-11 22:10:45 +0530

    Add Headroom to Webpack

--- core/templates/App.ts
+++ core/templates/App.ts
@@ -68,6 +68,8 @@ require('I18nFooter.ts');
 
 require('Polyfills.ts');
 
+require('headroom.import.ts');
+
 // TODO(#7222): Remove the following block of unnnecessary imports once
 // the code corresponding to the spec is upgraded to Angular 8.
 import { UpgradedServices } from 'services/UpgradedServices';
